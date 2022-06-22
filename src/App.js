91da89c1c17f119924902e4d7f706c9251cbdf06
salutJuillet <salut.juilllet@gmail.com>
2022-06-22 16:36:45 +0900

    axios 설치

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import Home0420 from './comp/Home0420';
 import Footer from './comp/Footer';
 import Write from './comp/Write';
 import Post from './comp/Post';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import todo from './data/data'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {  
   return (
     <BrowserRouter>
-        <Home0420 todo={todo} />
+        <Home0420 />
         {/* <Write /> */}
         {/* <Post /> */}
         <Footer />
 
       <Routes>
+        <Route path="/post/:id" element={<Post />} />
         <Route path="/write/*" element={<Write />} />
       </Routes>
     </BrowserRouter>
--- src/comp/Home0420.jsx
+++ src/comp/Home0420.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './home.css'
+import axios from 'axios'
 import { MdArrowLeft, MdArrowRight } from 'react-icons/md';
 import { BsPlus, BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import { HiPencil } from 'react-icons/hi';
 
 export default function Home(props) {
-    const todoList = [];
-    console.log(props.todo[0].apr20);
-    for(let list of props.todo[0].apr20){
-        todoList.push(<li key={list.id}>
-                        <input type="checkbox" /> 
-                        <span className="linkBox">
-                            <a href="#">{list.body}</a>
-                            {/* { ({list.body}="")?<Link to="/write">{list.body}</Link> : (<span><Link to="Home">Home</Link></span>)} */}
-                            <span className="editBtn"><a href="#"><HiPencil /></a></span>
-                        </span>
-                      </li>);
-    }
+
+    // const todoList = [];
+    // console.log(props.todo[0].apr20);
+    // for(let list of props.todo[0].apr20){
+    //     todoList.push(<li key={list.id}>
+    //                     <input type="checkbox" /> 
+    //                     <span className="linkBox">
+    //                         <a href="#">{list.body}</a>
+    //                         {/* { ({list.body}="")?<Link to="/write">{list.body}</Link> : (<span><Link to="Home">Home</Link></span>)} */}
+    //                         <span className="editBtn"><a href="#"><HiPencil /></a></span>
+    //                     </span>
+    //                   </li>);
+    // }
 
   return (
     <div className="wrapper">
@@ -36,7 +38,7 @@ export default function Home(props) {
 
             {/* 투두 리스트 */}
             <ul className="todo">
-                {todoList}
+                {/* {todoList} */}
             </ul>
 
             <p className="bottomText">Every day, good day!</p>
